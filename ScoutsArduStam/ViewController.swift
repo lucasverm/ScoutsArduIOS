dc79abac3617518a15f0ad6e7e8928e0bda4ffa9
lucasverm <lucasvermeulen@gmail.com>
2019-11-25 17:03:56 +0100

    Initial Commit

---
+++ ScoutsArduStam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScoutsArduStam
+//
+//  Created by Lucas Vermeulen on 25/11/2019.
+//  Copyright © 2019 Lucas Vermeulen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
